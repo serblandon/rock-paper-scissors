dc2d9341ce0b7235bacb6af673d6e99ae03c5a5d
SerbanCrasovan <serban.crasovan@student.upt.ro>
2022-11-10 12:22:40 +0200

    link basic html file to js and implement getcomputer choice fct

---
+++ jsscript.js
@@ -0,0 +1,16 @@
+const rps = ["Rock", "Paper", "Scissors"];
+
+// returns the random indice corresponding to rock paper or scissors
+function getComputerChoice() {
+    return rps[Math.floor(Math.random() * rps.length)];
+}
+
+// function for playing a single round
+function playRound(playerSelection, computerSelection) {
+
+    // if both participants select the same nothing happens and it's a draw
+    if(playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        console.log("It's a draw!");
+    }
+    
+}
